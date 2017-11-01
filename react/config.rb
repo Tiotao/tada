a187fe92225e4cb0754bba244723403a644ef5d8
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-11-01 10:39:08 -0700

    Add compass config

---
+++ react/config.rb
@@ -0,0 +1,13 @@
+
+http_path = '/'
+css_dir = "../../public/interface/styles"          #where the CSS will saved
+sass_dir = "/src"           #where our .scss files are
+images_dir = "../images"    #the folder with your images
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded # After dev :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+preferred_syntax = :sass 
+relative_assets = true
