e2dcf74d22ef5a730031008d811e33a68fcf7c71
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-11-01 15:51:47 -0700

    Connect to API

--- react/src/js/components/Layout.js
+++ react/src/js/components/Layout.js
@@ -1,22 +1,38 @@
 import React from "react";
+import axios from "axios";
 
 import Header from "./Header";
 import LeftBar from "./LeftBar";
 import Canvas from "./Canvas";
+import LabelStore from "../stores/LabelStore";
 
 export default class Layout extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: "Tada Interface",
+      title: "Tada Interface"
     };
   }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/labels')
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          data : res.data.data.slice(0,50)
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
 
   render() {
     return (
       <div>
         <Header title={this.state.title} />
-        <LeftBar />
+        <LeftBar data={this.state.data} />
         <Canvas />
       </div>
     );
--- react/src/js/components/LeftBar.js
+++ react/src/js/components/LeftBar.js
@@ -1,29 +1,24 @@
 import React from "react";
+import axios from "axios";
+
+import Label from "./Label";
 
 export default class LeftBar extends React.Component {
 	render() {
+		const labels = this.props.data;
+
+		let Labels 
+
+		if(labels) {
+			Labels = labels.map((label) => {
+				return <Label key={label._id} {...label}/>;
+			});
+			console.log(labels)
+		}
+
 		return (
 			<div class="LeftBar">
-				<ul class="LeftBarLabels">	
-					<a class="LeftBarLabel">
-						<li>
-							<p class="LeftBarLabelName">name1</p>
-							<p class="LeftBarLabelCount">count1</p>
-							</li>
-					</a>
-					<a class="LeftBarLabel">
-						<li>
-							<p class="LeftBarLabelName">name1</p>
-							<p class="LeftBarLabelCount">count1</p>
-							</li>
-					</a>
-					<a class="LeftBarLabel">
-						<li>
-							<p class="LeftBarLabelName">name1</p>
-							<p class="LeftBarLabelCount">count1</p>
-							</li>
-					</a>
-				</ul>
+				<ul class="LeftBarLabels">{Labels}</ul>
 			</div>
 		);
 	}
