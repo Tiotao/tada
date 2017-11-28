7c20697d563ee0b5db20f8199b62144710b8b505
Yiwen Zhang <tiotaocn@gmail.com>
2017-11-28 13:25:19 -0800

    update db

--- deploy/import_db.sh
+++ deploy/import_db.sh
@@ -9,21 +9,21 @@ echo "$inPath"
 
 
 echo Copying Labels...
-mongoimport -h "$host" -d tada-test -c label --file "$inPath"/label.json --mode upsert
+mongoimport -h "$host" -d tada-test -c label --file "$inPath"/label.json
 echo Labels Copied
 
 echo Copying Label Cache...
-mongoimport -h "$host" -d tada-test -c label_cache --file "$inPath"/label_cache.json --mode upsert
+mongoimport -h "$host" -d tada-test -c label_cache --file "$inPath"/label_cache.json
 echo Label Cache Copied
 
 echo Copying Stats...
-mongoimport -h "$host" -d tada-test -c stats --file "$inPath"/stats.json --mode upsert
+mongoimport -h "$host" -d tada-test -c stats --file "$inPath"/stats.json
 echo Stats Copied
 
 echo Copying Meta Labels...
-mongoimport -h "$host" -d tada-test -c meta_label --file "$inPath"/meta_label.json --mode upsert
+mongoimport -h "$host" -d tada-test -c meta_label --file "$inPath"/meta_label.json
 echo Meta Labels Copied
 
 echo Copying Videos...
-mongoimport -h "$host" -d tada-test -c video --file "$inPath"/video.json --mode upsert
+mongoimport -h "$host" -d tada-test -c video --file "$inPath"/video.json
 echo Videos Copied
