6d35c65beb5a1d7230a451005b0b113c7ce82198
Jiayang Zhang <jiayangzhang1104@gmail.com>
2017-12-04 17:53:20 -0800

    Fix click anywhere to close

--- react/src/js/components/Action.js
+++ react/src/js/components/Action.js
@@ -12,10 +12,24 @@ export default class Action extends React.Component {
 		var action = $(e.target).parent().attr('id'); 
 		if(action == "Add") {
 			$('.VideoActionAdd').removeClass('hidden').addClass('load');
+			$(document).click(function(e) {
+				if(!$(e.target).closest('.VideoActionAdd').length && !$(e.target).hasClass('ActionButtonIcon')) {
+					if(!$('.VideoActionAdd').hasClass('hidden')) {
+						$('.VideoActionAdd').addClass('hidden');
+					}
+				}
+			})
 		}
 		if(action == "Share") {
 			$('.VideoActionShare').removeClass('hidden').addClass('load');
 			// document.execCommand('copy');
+			$(document).click(function(e) {
+				if(!$(e.target).closest('.VideoActionShare').length && !$(e.target).hasClass('ActionButtonIcon')) {
+					if(!$('.VideoActionShare').hasClass('hidden')) {
+						$('.VideoActionShare').addClass('hidden');
+					}
+				}
+			})
 		}
 	}
 
