c465114b08ea6562777fd7c048576ffa3cb55dcb
Yiwen Zhang <tiotaocn@gmail.com>
2017-12-08 14:51:18 -0800

    modify build script

--- deploy/backup_db.sh
+++ deploy/backup_db.sh
@@ -3,26 +3,28 @@
 echo Export Database Star Wars Battlefront
 read -r -p 'DB host: ' host 
 echo "$host"
+read -r -p 'DB name: ' dbname
+echo "$dbname"
 read -r -p  'Output Path: ' InputOutPath
 outPath=$(echo "$InputOutPath" | sed -e 's#^J:##' -e 's#\\#/#g')
 echo "$outPath"
 
 echo Copying Labels...
-mongoexport -h "$host" -d tada-test -c label -o "$outPath"/label.json
+mongoexport -h "$host" -d "$dbname" -c label -o "$outPath"/"$dbname"_label.json
 echo Labels Copied
 
 echo Copying Label Cache...
-mongoexport -h "$host" -d tada-test -c label_cache -o "$outPath"/label_cache.json
+mongoexport -h "$host" -d "$dbname" -c label_cache -o "$outPath"/"$dbname"_label_cache.json
 echo Label Cache Copied
 
 echo Copying Stats...
-mongoexport -h "$host" -d tada-test -c stats -o "$outPath"/stats.json
+mongoexport -h "$host" -d "$dbname" -c stats -o "$outPath"/"$dbname"_stats.json
 echo Stats Copied
 
 echo Copying Meta Labels...
-mongoexport -h "$host" -d tada-test -c meta_label -o "$outPath"/meta_label.json
+mongoexport -h "$host" -d "$dbname" -c meta_label -o "$outPath"/"$dbname"_meta_label.json
 echo Meta Labels Copied
 
 echo Copying Videos...
-mongoexport -h "$host" -d tada-test -c video -o "$outPath"/video.json
+mongoexport -h "$host" -d "$dbname" -c video -o "$outPath"/"$dbname"_video.json
 echo Videos Copied
--- deploy/import_db.sh
+++ deploy/import_db.sh
@@ -3,32 +3,34 @@
 echo Import Database Star Wars Battlefront
 read -r -p 'DB host: ' host 
 echo "$host"
+read -r -p 'DB name: ' dbname
+echo "$dbname"
 read -r -p  'Input Path: ' InputInPath
 inPath=$(echo "$InputInPath" | sed -e 's#^J:##' -e 's#\\#/#g')
 echo "$inPath"
 
 
 echo Copying Labels...
-mongo "$host"/tada-test --eval 'db.label.drop()'
-mongoimport -h "$host" -d tada-test -c label --file "$inPath"/label.json
+mongo "$host"/"$dbname" --eval 'db.label.drop()'
+mongoimport -h "$host" -d "$dbname" -c label --file "$inPath"/"$dbname"_label.json
 echo Labels Copied
 
 echo Copying Label Cache...
-mongo "$host"/tada-test --eval 'db.label_cache.drop()'
-mongoimport -h "$host" -d tada-test -c label_cache --file "$inPath"/label_cache.json
+mongo "$host"/"$dbname" --eval 'db.label_cache.drop()'
+mongoimport -h "$host" -d "$dbname" -c label_cache --file "$inPath"/"$dbname"_label_cache.json
 echo Label Cache Copied
 
 echo Copying Stats...
-mongo "$host"/tada-test --eval 'db.stats.drop()'
-mongoimport -h "$host" -d tada-test -c stats --file "$inPath"/stats.json
+mongo "$host"/"$dbname" --eval 'db.stats.drop()'
+mongoimport -h "$host" -d "$dbname" -c stats --file "$inPath"/"$dbname"_stats.json
 echo Stats Copied
 
 echo Copying Meta Labels...
-mongo "$host"/tada-test --eval 'db.meta_label.drop()'
-mongoimport -h "$host" -d tada-test -c meta_label --file "$inPath"/meta_label.json
+mongo "$host"/"$dbname" --eval 'db.meta_label.drop()'
+mongoimport -h "$host" -d "$dbname" -c meta_label --file "$inPath"/"$dbname"_meta_label.json
 echo Meta Labels Copied
 
 echo Copying Videos...
-mongo "$host"/tada-test --eval 'db.video.drop()'
-mongoimport -h "$host" -d tada-test -c video --file "$inPath"/video.json
+mongo "$host"/"$dbname" --eval 'db.video.drop()'
+mongoimport -h "$host" -d "$dbname" -c video --file "$inPath"/"$dbname"_video.json
 echo Videos Copied
