a46aa836b44e854d3db3b63d656b29c69fe2fc36
Yiwen Zhang <tiotaocn@gmail.com>
2017-12-12 19:30:20 -0800

    add view comment

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,7 +4,11 @@ const logger  = require('logger').createLogger();
 const config = require('config');
 logger.setLevel(config.get("Logger.level"));
 
-
+/**
+ * Render Front end interface
+ * @param {Object} req - standard express request object
+ * @param {Object} res - standard express response object
+ */
 async function renderInterface(req, res) {
     res.render('../react/src/index', {
         title: "Tada interface prototype"
